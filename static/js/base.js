ef2fad6a47f63e9eeddabcb140ac3aeeb56aa357
Agnes <agnes.mccall5@gmail.com>
2024-07-03 22:05:04 -0400

    fix bug where current list doesn't show

--- static/js/base.js
+++ static/js/base.js
@@ -1,7 +1,13 @@
 $(document).ready(function() {
     console.log('started')
+
+    if (!localStorage.getItem('currentList')) {
+        const firstListTitle = $('#currentListMenu li:first-child a').text();
+        localStorage.setItem('currentList', firstListTitle);
+    }
+
     $('#currentList').text(localStorage.getItem('currentList')).show();
-});
+})
 
 $('#currentListMenu').click(function(evt) {
     localStorage.setItem('currentList', evt.target.innerText)
