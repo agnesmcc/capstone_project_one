a9a58f2f4efc258735a7b5722b8ce44f9e2251c2
Agnes <agnes.mccall5@gmail.com>
2024-07-23 22:20:39 -0400

    Fix seed file

--- seed.py
+++ seed.py
@@ -1,16 +1,16 @@
 import os
 from pprint import pprint
 
-from app import db
+from app import db, app
 from models import Recipe
 
 import spoonacular
 from spoonacular.models.search_recipes200_response import SearchRecipes200Response
 from spoonacular.rest import ApiException
 
-
-db.drop_all()
-db.create_all()
+# with app.app_context():
+#     db.drop_all()
+#     db.create_all()
 
 configuration = spoonacular.Configuration(
     host = "https://api.spoonacular.com"
@@ -23,7 +23,7 @@ with spoonacular.ApiClient(configuration) as api_client:
     number = 30
     try:
         # Search Recipes
-        api_response = api_instance.search_recipes(number=number)
+        api_response = api_instance.search_recipes(query='', number=number)
         print("The response of RecipesApi->search_recipes:\n")
         pprint(api_response)
     except Exception as e:
@@ -38,5 +38,6 @@ with spoonacular.ApiClient(configuration) as api_client:
         for recipe in api_response.results
     ]
 
-    db.session.bulk_save_objects(recipes)
-    db.session.commit()
+    with app.app_context():
+        db.session.bulk_save_objects(recipes)
+        db.session.commit()
